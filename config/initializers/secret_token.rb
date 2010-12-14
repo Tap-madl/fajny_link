a711c2d54ef10df2f65d2df21f839692b65c889b
dudek <dudzinski.michal@yahoo.com>
2010-12-14 20:42:05 +0100

    Rozpoczecie projektu na zaliczenie przedmiotu architektura serwisow internetowych.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FajnyLink::Application.config.secret_token = '81f707237aa12d40b46952ff0aef129c14fb380a253e02ff98b9fb35af93875cc49d698df40ce0f978e656cf4039fbb3c7402ab45bf4f0657cf8a14c35be4e69'
