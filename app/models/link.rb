377e805d8105703b92041e94019cf7f5e857d09c
mdudzins <mdudzins@sigma.ug.edu.pl>
2011-01-13 20:42:14 +0100

    Revert "Ostatnie przed nowym repo"
    This reverts commit 84449c5a46a340bae4eb08fe1d2cc14704f6db80.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,7 +1,8 @@
 class Link < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
-  attr_accessible :title, :url, :body, :user, :tag_list, :private
+  attr_accessible :title, :url, :body, :user, :tag_list, :count, :private
+
   acts_as_taggable_on :tags
   ActsAsTaggableOn::TagList.delimiter = " "
 
--- db/migrate/20110109000953_add_comments_amount_to_links.rb
+++
@@ -1,9 +0,0 @@
-class AddCommentsAmountToLinks < ActiveRecord::Migration
-  def self.up
-    add_column :links, :comments_amount, :integer
-  end
-
-  def self.down
-    remove_column :links, :comments_amount
-  end
-end
